e1faa5b737e6dbfd742e70fd723a734a4c654b0e
Salsrefil <240789@edu.p.lodz.pl>
2022-01-17 17:40:20 +0100

    2

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int Policz(int a, int b){
+    return a+b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("Wynik liczenia: %d", Policz(2,3));
     return 0;
 }
