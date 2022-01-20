c267f2e9a7f93f38b6d3da9adf741c5a326bf285
Salsrefil <240789@edu.p.lodz.pl>
2022-01-21 00:53:41 +0100

    Initial commit

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-int policz(int a,int b)
-{
-    return a*b;
-}
-
 int main() {
     printf("Hello, World!\n");
-    printf("Wynik liczenia: %d", policz(2,3));
     return 0;
 }
