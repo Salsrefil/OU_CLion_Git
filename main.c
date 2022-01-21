81a3d768c0de13d0767e80c73d035f1222286aa8
Salsrefil <240789@edu.p.lodz.pl>
2022-01-21 01:00:14 +0100

    Dodatkowe zmiany w 03

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int policz(int a, int b)
+{
+    return (a+b)*(a-b);
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("Wynik liczenia: %d", policz(2,3));
     return 0;
 }
